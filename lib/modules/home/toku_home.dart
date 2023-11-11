cabf2fcf1f3300181c8b2b1b9408f9d1eff23534
abdobassel <abdobassel@gmail.com>
2023-11-11 04:51:25 +0200

    create numbersScreen and create custom appBar and make categoris avilable on tap

--- lib/modules/home/toku_home.dart
+++ lib/modules/home/toku_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toku_app/components/components.dart';
+import 'package:toku_app/modules/numbers/numbers_screen.dart';
 
 class TokoHome extends StatelessWidget {
   const TokoHome({super.key});
@@ -14,10 +15,39 @@ class TokoHome extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Category(text: 'Numbers', color: Color(0xffFF9F3C)),
-          Category(text: 'Members Family', color: Color(0xff5E8A3F)),
-          Category(text: 'Colors', color: Color(0xff864CAF)),
-          Category(text: 'Phrases', color: Color(0xff52AFD6)),
+          Category(
+            text: 'Numbers',
+            color: Color(0xffFF9F3C),
+            ontap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return Numbers_screen();
+              }));
+            },
+          ),
+          Category(
+              text: 'Members Family',
+              color: Color(0xff5E8A3F),
+              ontap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Numbers_screen();
+                }));
+              }),
+          Category(
+              text: 'Colors',
+              color: Color(0xff864CAF),
+              ontap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Numbers_screen();
+                }));
+              }),
+          Category(
+              text: 'Phrases',
+              color: Color(0xff52AFD6),
+              ontap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Numbers_screen();
+                }));
+              }),
         ],
       ),
     );
---
+++ lib/modules/numbers/numbers_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:toku_app/components/components.dart';
+
+class Numbers_screen extends StatelessWidget {
+  const Numbers_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBarToku(title: 'Numbers'),
+        body: Container(
+          child: Center(child: Text('TEXT')),
+        ));
+  }
+}
